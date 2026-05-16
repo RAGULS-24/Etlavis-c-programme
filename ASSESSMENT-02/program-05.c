49c3ad9db4af0a0b9181330b6ba81ce32a886eb9
RAGUL S <ragulyash200@gmail.com>
2026-05-16 22:02:10 +0530

    Create program-05.c

---
+++ ASSESSMENT-02/program-05.c
@@ -0,0 +1,13 @@
+//Get a number from the user and check if the number is less than or equal to 50. If yes, then print 1; otherwise, print 0
+
+#include <stdio.h>
+int main (){
+    int a ;
+    scanf("%d",&a);
+    if (a<=50){
+        printf("1");
+    }
+    else {
+         printf("0");
+         }
+    }
