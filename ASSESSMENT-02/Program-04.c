02973f03c8f642e6e583f44dff97344519c1d3c4
RAGUL S <ragulyash200@gmail.com>
2026-05-16 13:29:35 +0530

    Add files via upload

---
+++ ASSESSMENT-02/Program-04.c
@@ -0,0 +1,13 @@
+//Get a number from the user and check if the number is greater than 50. If yes, then print 1; otherwise, print 0
+
+#include <stdio.h>
+int main (){
+    int a ;
+    scanf("%d",&a);
+    if (a>50){
+        printf("1");
+    }
+    else if (a<=50){
+         printf("0");
+        }
+    }
---
+++ ASSESSMENT-02/Program-08.c
@@ -0,0 +1,11 @@
+//Get a two-digit number from the user and check if the digit 0 and digit 1 are not identical. If yes, print 1; otherwise, print 0
+
+#include <stdio.h>
+int main (){
+    int a;
+    scanf("%d",&a);
+    if ((a%10)!=(a/10)) {
+        printf("1");
+    }
+    else printf("0");
+}
